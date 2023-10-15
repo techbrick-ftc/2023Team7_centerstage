c87ad5298aa8ea1ffd0da2ea36b6f6da7e90fce4
Brickbuilder0658 <aidanfleischer@gmail.com>
2023-10-15 13:31:30 -0400

    removed roadrunner, added AutoDrive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class AutoDrive {
+    public DcMotor frontLeft;
+    public DcMotor backLeft;
+    public DcMotor frontRight;
+    public DcMotor backRight;
+
+    public DcMotorEx deadPerp;
+
+    public DcMotorEx deadLeft;
+
+    public DcMotorEx deadRight;
+
+
+
+    AutoDrive(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight, DcMotorEx deadPerp, DcMotorEx deadLeft, DcMotorEx deadRight){
+
+    }
+}
