18a587feed801ca3fc3ca9e770d75d52c80ddc95
Brickbuilder0658 <aidanfleischer@gmail.com>
2023-10-17 21:10:09 -0400

    Odometry wheels reset and setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
@@ -1,23 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.onbotjava.OnBotJavaManager.initialize;
+
+import android.graphics.Paint;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
-public class AutoDrive {
-    public DcMotor frontLeft;
-    public DcMotor backLeft;
-    public DcMotor frontRight;
-    public DcMotor backRight;
-
-    public DcMotorEx deadPerp;
-
-    public DcMotorEx deadLeft;
-
-    public DcMotorEx deadRight;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+@Autonomous(name = "ticks")
+public class AutoDrive extends StarterAuto {
 
 
-
-    AutoDrive(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight, DcMotorEx deadPerp, DcMotorEx deadLeft, DcMotorEx deadRight){
+    @Override
+    public void runOpMode() {
+        TelemetryPacket packet = new TelemetryPacket();
+        initialize();
+        waitForStart();
+       double x= deadLeft.getCurrentPosition();
+        double y= deadRight.getCurrentPosition();
+        double z= deadPerp.getCurrentPosition();
+        packet.put("deadLeft",x);
+        packet.put("deadRight",y);
+        packet.put("deadPerp",z);
+        dashboard.sendTelemetryPacket(packet);
 
     }
-}
+    }
+
