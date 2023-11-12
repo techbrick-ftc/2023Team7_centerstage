6ac77c14019ceefa8ab03736af92f7131214dd99
Charles Epple <charlieepp06@gmail.com>
2023-11-12 11:12:40 -0500

    Reupdated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterAuto.java
@@ -431,7 +431,7 @@ public class StarterAuto extends LinearOpMode {
         protected void armMove ( double leftStickX){
             armMotor.setPower(leftStickX);
         }
-
+//comment
         @Override
         public void runOpMode () {
 
