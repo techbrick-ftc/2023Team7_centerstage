ed6814517232944834e842a29ab7b7341f56bdab
Charles Epple <charlieepp06@gmail.com>
2023-11-16 19:53:41 -0500

    Lifting Arm and Finger Servo Moving in Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterAuto.java
@@ -122,6 +122,9 @@ public class StarterAuto extends LinearOpMode {
     public Servo armFlipper;
     //Control Hub port 1
     public Servo finger;
+    int[] servoPositions = {0,-45,-90};
+    int currentPosition = 2;
+    String increasingPosition = "increase";
     public ColorSensor colorFR;
     public ColorSensor colorFL;
     public ColorSensor colorBR;
@@ -372,7 +375,7 @@ public class StarterAuto extends LinearOpMode {
             colorBL = hardwareMap.colorSensor.get("colorBL");
             armFlipper = hardwareMap.servo.get("armFlipper");
             finger = hardwareMap.servo.get("finger");
-
+            setFinger(servoPositions[2]);
             imu = hardwareMap.get(IMU.class, "imu");
             backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -454,6 +457,9 @@ public class StarterAuto extends LinearOpMode {
                 lifterMotor.setPower(-1);
             }
             lifterMotor.setPower(0);
+        }
+        protected void setFinger(double degree){
+            finger.setPosition(degree);
         }
 //comment #2
         @Override
